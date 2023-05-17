071319cde6278f1fe189a98fb575c2cea34d99a0
Adamov Chingiskhan <104496869+ChingizAdamov@users.noreply.github.com>
2023-05-17 21:31:51 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,3 @@
-<?php
-   session_start();
-?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -11,7 +8,8 @@
    <link rel="stylesheet" href="assets/css/main.css">
 </head>
 <body>
-   <!-- form of autorization -->
+
+   <!-- Авторизация -->
 
    <form action="vendor/signin.php" method="post">
       <label>Введите телефон или почту</label>
@@ -23,12 +21,58 @@
          Нет аккаунта? - <a href="register.php">Зарегистрируйся</a>
       </p>
 
-      <?php
-         if ($_SESSION['message']) {
-            echo '<p class="msg"> ' . $_SESSION['message'] .' </p>';
-         }
-         unset($_SESSION['message']);
-         ?>
-      </form>
+       <!-- капча -->
+      <script src="https://smartcaptcha.yandexcloud.net/captcha.js" defer></script>
+
+<div
+   style="height: 100px;"
+    id="captcha-container"
+    class="smart-captcha"
+    data-sitekey="<ysc1_4PA9KeliApVK810BymfUPIXJ9LW3VJWR0QiV7wMVa4a83c63>"
+>
+</div>
+
+<div id="captcha-container" class="smart-captcha" ...>
+    <input type="hidden" name="smart-token" value="<токен>">
+    ...
+</div>
+
+   </form>
+      
 </body>
 </html>
+<?php
+define('SMARTCAPTCHA_SERVER_KEY', '<******>');
+
+function check_captcha($token) {
+    $ch = curl_init();
+    $args = http_build_query([
+        "secret" => SMARTCAPTCHA_SERVER_KEY,
+        "token" => $token,
+        "ip" => $_SERVER['REMOTE_ADDR']
+    ]);
+    curl_setopt($ch, CURLOPT_URL, "https://smartcaptcha.yandexcloud.net/validate?$args");
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_TIMEOUT, 1);
+
+    $server_output = curl_exec($ch);
+    $httpcode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+    curl_close($ch);
+
+    if ($httpcode !== 200) {
+        echo "Allow access due to an error: code=$httpcode; message=$server_output\n";
+        return true;
+    }
+    $resp = json_decode($server_output);
+    return $resp->status === "ok";
+}
+
+$token = $_POST['smart-token'];
+if (check_captcha($token)) {
+    echo "Passed\n";
+} else {
+    echo "Robot\n";
+}
+
+
+
